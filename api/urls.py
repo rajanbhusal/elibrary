52e44802f6e6418bb5111c21d50cd38d8f01566e
Rajan Bhusal <68371083+rajanbhusal@users.noreply.github.com>
2024-02-01 15:54:01 +0545

    Updated urls for each API endpoint

--- api/urls.py
+++ api/urls.py
@@ -6,4 +6,10 @@ urlpatterns = [
     path('create_user/', views.create_user, name="create_user"),
     path('all_users/', views.all_users, name="all_users"),
     path('get_user/', views.get_user, name="get_user"),
+    path('add_book/', views.add_book, name="add_book"),
+    path('all_books/', views.all_books, name="all_books"),
+    path('get_book/', views.get_book, name="get_book"),
+    path('update_book/', views.update_book, name="update_book"),
+    path('borrow_book/', views.borrow_book, name="borrow_book"),
+    path('borrowed_books/', views.borrowed_books, name="return_book"),
 ]
